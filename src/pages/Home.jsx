5e60492f4df25d6897511945b5797ad165f2415a
SIRTEE <sirtee244@gmail.com>
2025-02-04 16:03:41 +0700

    deploy2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <ContentCarousel />
 
       {/* สินค้าขายดี */}
-      <p className="text-2xl font-bold text-center my-6">สินค้าขายอย่างดี</p>
+      <p className="text-2xl font-bold text-center my-6">สินค้าขายอย่าง</p>
       <BestSeller />
 
       {/* สินค้าใหม่ */}
